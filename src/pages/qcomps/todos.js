b3a60be062b03daff727edd4bc8355bf1dcacdad
Floria289 <floria289@hotmail.com>
2023-10-12 20:43:09 -0400

    fix

--- src/pages/qcomps/todos.js
+++ src/pages/qcomps/todos.js
@@ -1,22 +1,18 @@
-const baseUrl = 'https://i.imgur.com/';
-const person = {
-  name: 'Gregorio Y. Zara',
-  imageId: '7vQD0fP',
-  imageSize: 's',
-  theme: {
-    backgroundColor: 'black',
-    color: 'pink'
-  }
-};
-
 export default function TodoList() {
+  const baseUrl = "https://i.imgur.com/";
+  const person = {
+    name: "Gregorio Y. Zara",
+    imageId: "7vQD0fP",
+    imageSize: "s",
+    theme: {
+      backgroundColor: "black",
+      color: "pink",
+    },
+  };
   return (
     <div style={person.theme}>
-      <h1>Person Name's Todos</h1>
-      <img className="avatar"
-      src="URL"
-      alt="Person's Name"
-      />
+      <h1>{person.name}'s Todos</h1>
+      <img className="avatar" src={baseUrl + person.imageId + person.imageSize} alt={person.name} />
     </div>
   );
 }
